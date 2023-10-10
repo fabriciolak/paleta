61f8f869c6dce6827cbda6d2e2219fa67c6a1e6e
Fabricio <fabriciolak12@gmail.com>
2023-10-10 10:39:53 -0300

    feat: setup basics configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,21 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    colors: {
+      'cod-gray': {
+        '50': '#f6f6f6',
+        '100': '#e7e7e7',
+        '200': '#d1d1d1',
+        '300': '#b0b0b0',
+        '400': '#888888',
+        '500': '#6d6d6d',
+        '600': '#5d5d5d',
+        '700': '#4f4f4f',
+        '800': '#454545',
+        '900': '#3d3d3d',
+        '950': '#121212',
+      }
+    }
   },
   plugins: [],
 }
