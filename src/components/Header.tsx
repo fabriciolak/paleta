7280d5d9ff740c27c47b824fa86cd6d872524ace
Fabricio <fabriciolak12@gmail.com>
2023-10-20 11:07:27 -0300

    refactor: use semantic html tag

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,19 +2,19 @@ import Logo from "@/components/Logo";
 
 export function Header() {
   return (
-    <div className="w-full h-14 px-6 py-2 flex items-center justify-center border border-b-cod-gray-400/20">
-      <div className="w-[1280px] flex items-center justify-between">
+    <header className="w-full h-14 px-6 py-2 flex items-center justify-center border-b border-b-cod-gray-400/20">
+      <nav className="w-[1280px] flex items-center justify-between">
         <Logo />
 
-        <nav className="flex gap-2">
+        <div className="flex gap-2">
           <button className="h-10 px-6 py-2 flex items-center">
             Browse all
           </button>
           <button className="h-10 px-6 py-2 flex items-center bg-cod-gray-950 text-cod-gray-50 rounded-lg">
             Generate Palette
           </button>
-        </nav>
-      </div>
-    </div>
+        </div>
+      </nav>
+    </header>
   );
 }
